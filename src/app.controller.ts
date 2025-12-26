018139bcefe9723b7629ff72d089ae41a2903d87
thiagohrcosta <thiagohrcosta86@gmail.com>
2025-12-26 07:28:59 -0300

    Adding prisma setup

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { PrsimaService } from './prisma/prisma.service';
 
-@Controller()
+@Controller('api/v1')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly prismaService: PrsimaService,
+  ) {}
 
-  @Get()
-  getHello(): string {
+  @Get('/hello')
+  index(): string {
     return this.appService.getHello();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrsimaService } from './prisma/prisma.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrsimaService],
 })
 export class AppModule {}
