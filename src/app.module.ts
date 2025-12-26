36312524179355c675fa9a98547a0f338edfdd45
thiagohrcosta <thiagohrcosta86@gmail.com>
2025-12-26 15:12:30 -0300

    env configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { CreateAccountController } from './controllers/create-account-controller';
+import { ConfigModule } from '@nestjs/config'
+import { envSchema } from './env';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+    validate: env => envSchema.parse(env),
+    isGlobal: true,
+})],
   controllers: [
     AppController,
     CreateAccountController
